7460331112c7e5b2c0bf24ae0be7ab0979ae88fa
akil-07 <akilsudhagar7@gmail.com>
2025-11-02 22:29:29 +0530

    Add files via upload

---
+++ calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace program
+{
+    class calculator
+    {
+        static void Main()
+        {
+            Console.Write("Enter the first number = ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number = ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int add = x + y;
+            int sub = x - y;
+            int mul = x * y;
+            int div = x / y;
+            Console.WriteLine("The addition is " + add);
+            Console.WriteLine("The subraction is " + sub);
+            Console.WriteLine("The multiplication is " + mul);
+            Console.WriteLine("the division is " + div);
+            Console.ReadKey();
+        }
+    }
+}
