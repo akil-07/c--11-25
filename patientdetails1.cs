e0fdb298cd49c63cc54a4c0203b147faec873d2b
akil-07 <akilsudhagar7@gmail.com>
2025-11-05 00:35:48 +0530

    Add files via upload

---
+++ patientdetails1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter patient name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter room charge: ");
+        double roomCharge = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter medicine cost: ");
+        double medicineCost = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter doctor fee: ");
+        double doctorFee = Convert.ToDouble(Console.ReadLine());
+
+        double totalCost = roomCharge + medicineCost + doctorFee;
+
+        Console.WriteLine("\n--- Patient Bill ---");
+        Console.WriteLine("Patient Name: " + name);
+        Console.WriteLine("Total Cost: ₹" + totalCost);
+    }
+}
