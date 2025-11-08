6f899ed9c4b79489135a7c6bf40e86b22afecbcf
akil-07 <akilsudhagar7@gmail.com>
2025-11-08 10:49:44 +0530

    Add files via upload

---
+++ stringpraac.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sting=");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter a number in **-**-** format");
+            string num = Console.ReadLine();
+
+            string cap = name.ToUpper();
+            Console.WriteLine(cap);
+            string low = name.ToLower();
+            Console.WriteLine(low);
+            string rep = num.Replace("-", "/");
+            Console.WriteLine(rep);
+            string sub = name.Substring(0,3);
+            Console.WriteLine(sub);
+        }
+
+    }
+
+}
