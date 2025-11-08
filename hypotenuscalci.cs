cf8bbafcd8c4bfc0442d17f785e9d0ec278cdadf
akil-07 <akilsudhagar7@gmail.com>
2025-11-08 10:26:35 +0530

    Add files via upload

---
+++ hypotenuscalci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace practce
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Side A:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter The Side B:");
+            double b= Convert.ToDouble(Console.ReadLine());
+            double c = Math.Sqrt((a * a) + (b * b));
+            Console.WriteLine("the length of c is:"+c);
+            double area = 0.5 * a * b;
+            double peri = a + b + c;
+            Console.WriteLine($"The area is {area}");
+            Console.WriteLine($"The perimeter is {peri}");
+            Console.ReadKey();
+        }
+    }
+}
+    
