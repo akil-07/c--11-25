e5a95fad50adf4234f4c0a1fe98e7b963492d991
akil-07 <akilsudhagar7@gmail.com>
2025-11-08 20:15:48 +0530

    Add files via upload

---
+++ object.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace obj
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            human human1= new human();
+            human human2 = new human();
+            human1.name = "Akil";
+            human1.age = 18;
+        }
+    }
+    class human()
+    {
+        public int age;
+        public string name;
+        public void eat()
+        {
+            Console.WriteLine($"{name} is eating");
+        }
+        
+        public void sleeping()
+        {
+            Console.WriteLine(name + "is sleeping");
+        }
+    
+    }
+}
