29f76ab4a9cf99eb4ca5de9953e832bdc044d5c4
akil-07 <akilsudhagar7@gmail.com>
2025-11-08 15:27:08 +0530

    Add files via upload

---
+++ exceptions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace exceptions
+{
+    class program
+    {
+        static void Main()
+        {
+            double x;
+            double y;
+            double result;
+            try
+            {
+                Console.WriteLine("Enter number 1:");
+                x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter number 2:");
+                y = Convert.ToInt32(Console.ReadLine());
+                result = x / y;
+                Console.WriteLine("result = " + result);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Enter only numbers please");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("You can't divide a number by Zero");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong!!!");
+            }
+            
+        }
+    }
+    
+}
