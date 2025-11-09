7d9beafe2f7812294aa298d2ccf313a1e44e4a10
akil-07 <akilsudhagar7@gmail.com>
2025-11-09 10:10:58 +0530

    Add files via upload

---
+++ constructors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace constructors
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+           car car1 = new car("Mustang");
+            car car2 = new car("Lambo");
+            Console.WriteLine(car.numberofcars);
+        }
+    }
+    class car
+    {
+        String model;
+        public static int numberofcars;
+        public car(String model)
+        {
+            this.model = model;
+            numberofcars++;
+        }
+    }
+}    
