4aa3e3a88dfda7bbd01fc0cf77b6a7502792856b
akil-07 <akilsudhagar7@gmail.com>
2025-11-11 15:59:47 +0530

    Add files via upload

---
+++ list.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+namespace list
+{
+    class program
+    { 
+        static void Main()
+        {
+            List<string> fruits = new List<string>() { "Apple", "Banana", "Mamgo" };
+            Console.WriteLine("Fruits list:");
+            foreach(string fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+    }
+
+}
